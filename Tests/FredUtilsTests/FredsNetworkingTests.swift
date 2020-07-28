b34b7eb98c87f957abd951a467a67e6c727f0745
Fred Waltman <fwaltman@gmail.com>
2020-07-28 15:40:06 -0700

    Added first networking public API call

--- Tests/FredUtilsTests/FredsNetworkingTests.swift
+++ Tests/FredUtilsTests/FredsNetworkingTests.swift
@@ -6,16 +6,37 @@
 //
 
 import XCTest
+@testable import FredUtils
 
 final class FredsNetworkingTests: XCTestCase {
 
-   
-     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoadDataCall() {
+         
+        let manager = FredUtils.Networking.Manager()
+        let expectation = XCTestExpectation(description: "called for data"
+        )
+        
+        guard let url = URL(string: "https://FredWaltman.com") else {
+            return XCTFail("Could not create URL properly")
+        }
+        
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData) :
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error making error")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+        
     }
+   
+
 
     static var allTests = [
-        ("textExample", testExample)
+        ("testLoadDataCall", testLoadDataCall)
     ]
 }
